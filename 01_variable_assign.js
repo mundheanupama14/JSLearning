5eff23bb735d07d0e1790c5c51482f45244c3644
mundheanupama14 <mundheanupama14@gmail.com>
2023-02-16 10:31:14 -0800

    Created & Updated file with 01_variable_assign.js
    Created & Updated file with 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,69 @@
+console.log("********** STEP-1 **********");
+
+console.log("....Variable Declaration....");
+var myFullName // Variable Declaration
+console.log("My Full Name Is:",myFullName);
+
+console.log("....Variable Initialization....");
+myFullName = "Anupama Mundhe"; // Variable Initialization
+console.log("My Full Name Is:", myFullName);
+
+console.log("....Variable value Updated....");
+myFullName = "Anupama Ashok Mundhe"; // Variable Value Updated
+console.log("My Full Name Is:",myFullName);
+
+console.log("********** STEP-2 **********");
+
+console.log("....Single Variable Declaration and Initialization in same line....");
+var myLove = "I LOVE Only JS"; // Variable Declaration and Initialization in same line
+console.log("My Love:", myLove);
+
+console.log("********** STEP-3 **********");
+
+console.log("....Multiple Variables Declaration and Initialization in same line....")
+var firstName = "Anupama"; // Multiple Variables Declaration and Initialization in same line
+var lastName = "Mundhe";
+var age = "22";
+var address = "Jalna"; 
+var pincode = "431001"; 
+var gender = "Female";
+var isMarried = "No";
+console.log("First Name:",firstName, "Last Name:",lastName, "Age:",age, "City:",address, "Pincode:", pincode, 
+"Gender:",gender, "Marrital Status Is Married:",isMarried);
+
+
+
+
+
+
+
+
+// console.log("********** STEP-1 **********");
+// var city; // Variable Declaration
+// console.log(city);
+// city = "Pune"; // Variable Initialization
+// console.log(city);
+// city = "Surat"; // Variable Value Upadated
+// console.log(city);
+
+// console.log("********** STEP-3 **********");
+// var firstName = "Anupama"; // Variable Declaration and Initialization in same line
+// console.log("My First Name:", firstName);
+
+// var lastName = "Mundhe"; // Variable Declaration and Initialization in same line
+// console.log("My Last Name:", lastName);
+
+// var age = "22"; // Variable Declaration and Initialization in same line
+// console.log("My Age:", age);
+
+// var address = "Jalna"; // Variable Declaration and Initialization in same line
+// console.log("My City:", address);
+
+// var pincode = "431001"; // Variable Declaration and Initialization in same line
+// console.log("Pincode:", pincode);
+
+// var gender = "Female"; // Variable Declaration and Initialization in same line
+// console.log("Gender Is:", gender);
+
+// var isMarried = "No"; // Variable Declaration and Initialization in same line
+// console.log("Marrital Status Is Married:", isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
-console.log("Mohit Sharma")
+console.log("Mohit Sharma");
+console.log("31");
+console.log("Pune");
+
+var fullName = "Anupama Mundhe";
+console.log(fullName);
+console.log(fullName);
+
+var city; // Variable Declaration
+console.log(city);
+city = "Pune"; // Variable Initialization
+console.log(city);
+city = "Surat"; // Variable Value Updation
+console.log(city);
