af3654a9fbab4540192e4ec09083584177b474f9
mundheanupama14 <mundheanupama14@gmail.com>
2023-02-23 20:38:37 -0800

    Created File with file name 05_terOperatoraAsig0A.js, 05_terOperatorAssig0B.js, 05_terOperatorAssig0C.js and also Updated file 05_operator .js
    Created File with file name 05_terOperatorAssig0A.js, 05_terOperatorAssig0B.js, 05_terOperatorAssig0C.js and also  Updated 05_operator.js.

--- 05_operator.js
+++ 05_operator.js
@@ -44,14 +44,72 @@ console.log(`====Arithmetic Operator====`);
 
  var num = 10; // Modulus
  num%=3; // num = num % 3 
+
  console.log(`Compaound Reminder Is: ${num}`);
 
 
-
-
-
-console.log(`Caparison operators `);
+ console.log(`Caparison operators `);
  var num3 = "10";
  var num4 = 10;
  console.log(num3==num4); // 10==10
  console.log(num3===num4);
+
+
+ var marks = 35;
+ console.log( marks >= 35); // Greater than equal to >=
+ // Is marks Greater than 35
+ // Is marks Greater than Equal to 35
+
+
+ var marks = 35;
+ console.log( marks <= 35); // Less than equal to <=
+ // Is marks Less than 35
+ // Is marks Less than Equal to 
+
+
+ var marks = 70;
+ var result = marks >= 60 ? "Allow Him for Interview" : "Don't Allow";
+ console.log(result);
+
+ var age = 20;
+ var res = age >= 21 ? true : false;
+ console.log(res);
+
+
+ console.log("Even or Odd");
+ var myNumber = 7;
+ // Even - Divide by 2 and reminder is 0
+console.log(myNumber %2== 0);
+var result = myNumber %2== 0 ? "Even" : "Odd";
+console.log(result);
+
+var num1 = 10;
+var num2 = 12;
+var result = num1 > num2 ? num1 : num2;
+console.log(result);
+
+
+// Function with One 1 Argument and Return Value
+
+function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength %2== 0 ? "Even" : "Odd";
+    return result;
+}
+var result = wordLength("JavaScript");
+console.log(`word "JavaSript" has ${result} length`);
+
+// var result = wordLength("Google Chrome");
+// console.log(`word "Google Chrome" has ${result} length`);
+
+console.log(`word "Google Chrome" has ${wordLength("Google Chrome")} length`); // Written in Single Line code without using var result variable to strore result
+
+
+function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are Eligible for Marriage` : "Not Eligible Try next time";
+    return result; 
+
+}
+var result = maleMarriageEligibility("Male", 25, "Billgates");
+console.log(result);
+
