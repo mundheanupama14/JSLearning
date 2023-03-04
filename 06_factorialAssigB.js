050a5b929a99ebe95da6c0b121e949c5dbe8e1be
mundheanupama14 <mundheanupama14@gmail.com>
2023-03-04 10:34:44 -0800

    Update file 06_factorialAssig.js
     Update file 06_factorialAssig.js

--- 06_factorialAssigB.js
+++ 06_factorialAssigB.js
@@ -1,33 +1,72 @@
-function factorialOfWordsCount(word){
+console.log("*********** Actual-EXpected Assignment Program of 06_factorialAssigB ***********");
 
-    let count = 1;
-    let factorial = 1;
+function factorialOfWordsCount(str) {
 
-    console.log(`The String is: ${word}`);
+    if (str == null || str == undefined || isNaN(str) && typeof str == "number") {
 
-    if (word == null || word == undefined) {
-
-        console.log(`Please enter the valid input ${word} is Invalid`);
+        console.log(`Given String ${str} Is Invalid`);
     }
     else {
 
-        for (let index = 0; index <= word.length; index++) {
-            var char = word.charAt(index)
-            if (char == " ") {
-                count = count + 1;
-                factorial = factorial * count;
-            }
+        console.log(`Given String Is: ${str}`);
+
+        let word = str.split(" ");
+        const totalWords = word.length;
+        console.log(`Toatal number of words in Given String: ${str} Is: ${totalWords}`);
+
+        // 6! -> 6*5*4*3*2*1 = 120
+        let factorial = 1; // 120
+        for (let index = totalWords; index >= 1; index--) { // 4
+
+            factorial = factorial * index;
         }
-        return factorial;
-}
+        // console.log(`Factorial of ${str} is ${factorial}`);
+        return `Factorial of ${str} is ${factorial}`;
+    }
 }
 var result = factorialOfWordsCount("Revision is the mother of Success");
-console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+console.log(result);
 var result = factorialOfWordsCount("We never fail, we always learn");
-console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+console.log(result);
 var result = factorialOfWordsCount(null);
-//console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+//console.log(result);
 var result = factorialOfWordsCount("");
-console.log(`Total numbers of words available in the string and its factorial is:${result}`);
-var result = factorialOfWordsCount("Anupama Mundhe");
-console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+console.log(result);
+var result = factorialOfWordsCount("Anupama Ashok Mundhe");
+console.log(result);
+
+
+
+// function factorialOfWordsCount(word) {
+
+//     let count = 1;
+//     let factorial = 1;
+
+//     console.log(`The String is: ${word}`);
+
+//     if (word == null || word == undefined) {
+
+//         console.log(`Please enter the valid input ${word} is Invalid`);
+//     }
+//     else {
+
+//         for (let index = 0; index <= word.length; index++) {
+//             var char = word.charAt(index)
+//             if (char == " ") {
+//                 count = count + 1;
+//                 factorial = factorial * count;
+//             }
+//         }
+//         return factorial;
+//     }
+// }
+// var result = factorialOfWordsCount("Revision is the mother of Success");
+// console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+// var result = factorialOfWordsCount("We never fail, we always learn");
+// console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+// var result = factorialOfWordsCount(null);
+// //console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+// var result = factorialOfWordsCount("");
+// console.log(`Total numbers of words available in the string and its factorial is:${result}`);
+// var result = factorialOfWordsCount("Anupama Mundhe");
+// console.log(`Total numbers of words available in the string and its factorial is:${result}`);
